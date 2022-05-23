41650b860f7ad5bcb63ed077088486431a3e7e45
badli <badli@LAPTOP-1C38NT4G>
2022-05-23 15:12:24 +0530

    new proj

---
+++ corejava/src/abc_package/addition.java
@@ -0,0 +1,12 @@
+package abc_package;
+
+public class addition {
+
+	public int addtwonum(int a, int b)
+	{
+	
+		return a+b;
+
+	}
+
+}
---
+++ corejava/src/xyz_package/test.java
@@ -0,0 +1,15 @@
+package xyz_package;
+import abc_package.*;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		addition a1=new addition();
+		
+		a1.addtwonum(1, 2);
+		System.out.println(a1.addtwonum(1, 2));
+	}
+
+}
