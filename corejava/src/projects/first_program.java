1f8dff51f297b09007f3160ee0d6dd86a6171cce
badli <badli@192.168.0.6>
2022-05-23 13:47:53 +0530

    First Commit

---
+++ corejava/src/projects/first_program.java
@@ -0,0 +1,11 @@
+package projects;
+
+public class first_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short value = 32000;
+		System.out.println("A short:" + value);
+	}
+
+}
